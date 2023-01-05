bd2e72710b80446731873f96a7d21896ee2d45b5
madhu-ibs <113244676+madhu-ibs@users.noreply.github.com>
2023-01-05 12:31:25 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MRPFD-GAMO'
+copyright = '2022, Impressico Business Solutions'
+author = 'IBS'
 
 release = '0.1'
 version = '0.1.0'
